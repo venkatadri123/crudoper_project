e641ec1abcef06ca6b05fbb3f6d6ef9185505e31
MAC <venkatadrimanubolu@gmail.com>
2020-05-29 13:59:59 +0530

    renamed project

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crud_pro.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crudoper_project.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
